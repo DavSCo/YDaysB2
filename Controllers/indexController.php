a2a8845cd305af07fec80fa712e73bf7ed951f62
SamuelMrejen <samuel.mrejen@ynov.com>
2017-12-14 11:25:54 +0100

    test

---
+++ Controllers/indexController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shmouel
+ * Date: 14/12/2017
+ * Time: 11:25
+ */
---
+++ Models/UserSQL.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Shmouel
+ * Date: 14/12/2017
+ * Time: 11:25
+ */
+class UserSQL
+{
+
+}
