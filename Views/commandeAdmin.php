18cd27bf90814be22f350c009340dcc54756d7ea
SamuelMrejen <samuel.mrejen@ynov.com>
2017-12-24 14:55:36 +0100

    css

--- Views/commandeAdmin.php
+++ Views/commandeAdmin.php
@@ -1,33 +1,4 @@
 <?php require_once ('Views/layoutAdmin.php');?>
-<div class="content-wrapper">
-    <div class="container-fluid">
-        <!-- Breadcrumbs-->
-        <!-- Icon Cards-->
-        <div class="row">
-            <div class="col-xl-3 col-sm-6 mb-3">
-
-                <?php   for ($i=0;$i<count($selectAllGlasses); $i++){?>
-                    <div class="local" id="local">
-                        <div class="col-xs-2 col-xs-offset-1 table-caption">
-                            <div class="card h-100">
-                                <a href="#"><img class="card-img-top"  src="<?= $selectAllGlasses[$i]['photo'] ?>" alt=""></a>
-                                <div class="card-body">
-                                    <h1><?= $selectAllGlasses[$i]['ref'] ?></h1>
-                                    <h4 class="card-title">
-                                        <a href="#"><?= $selectAllGlasses[$i]['user_id'] ?></a>
-                                    </h4>
-                                    <p class="card-text"><?= $selectAllGlasses[$i]['glasses_id'] ?></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
-
-
-</div>
 
 
 <div class="col-lg-9">
@@ -36,7 +7,7 @@
     </div>
 
     <div class="row">
-        <?php foreach ($selectAllGlasses as $item) { ?>
+        <?php   for ($i=0;$i<count($selectAllGlasses); $i++){?>
 
             <div class="col-lg-4 col-md-6 mb-4">
                 <div class="card h-100">
